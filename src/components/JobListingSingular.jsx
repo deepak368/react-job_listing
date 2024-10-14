a3fc084222a9a9b8135bdba71fcdb7ebc7ccfa15
Deepak <deepakdinesh368@gmail.com>
2024-10-14 18:09:44 +0530

    created jobs page and the error page

--- src/components/JobListingSingular.jsx
+++ src/components/JobListingSingular.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const JobListingSingular = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -27,10 +28,10 @@ const JobListingSingular = ({ job }) => {
                     <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                     { job.location }
                     </div>
-                    <a
-                    href={`/job/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                    <Link
+                    to={`/job/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
-                    </a>
+                    </Link>
                 </div>
                 </div>
             </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     <>
     <Hero/>
     <Homecard />
-    <Joblisting />
+    <Joblisting isHome={true} />
     <ViewAllJobs />
     </>
   )
