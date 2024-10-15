c2ce91a676e228ca347465bd16e510db67533750
Deepak <deepakdinesh368@gmail.com>
2024-10-15 22:52:49 +0530

    used the backed js in the main component and the useEffect hook

---
+++ react-app/src/App.tsx
@@ -0,0 +1,7 @@
+// importing the messsage component from MessageChannel.tsx 
+import Messsage from "./Message"
+
+function App(){
+  return <div><Messsage></Messsage></div>
+}
+export default App
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,7 @@
+// component creation using js funtion
+function Messsage(){
+    // we can use html tags this process is called javascript xml
+    return <h1>Hellow world</h1>
+}
+// we must export it to use it in a difrent file 
+export default Messsage
