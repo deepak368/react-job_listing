666fc5273fcf1a49b8895bdaa9dee81f13d5a3da
Deepak <deepakdinesh368@gmail.com>
2024-11-25 15:50:58 +0530

    add the delete functionality for the job

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,41 @@ import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
-import NotFound from './pages/NotFound'
-
+import NotFound from './pages/NotFound'  
 import JobPage,{jobLoader} from './pages/Jobpage'
 import AddJobPage from './pages/AddJobPage'
 
 
-const router = createBrowserRouter(createRoutesFromElements(
-<Route path='/' element={<MainLayout />}>
-  <Route index element={<HomePage/>}/>
-  <Route path='/jobs' element={<JobsPage />} />
-  <Route path='/add-job' element={<AddJobPage />} />
-  <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader } />
-  <Route path='*' element={<NotFound />} />
-
-</Route>)
-);
 
 const App = () => {
+  // add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs',{
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  }
+  //delete the job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: 'DELETE'
+    });
+    return;
+  };
+  
+  const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage/>}/>
+    <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+    <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob }/>} loader={jobLoader } />
+    <Route path='*' element={<NotFound />} /> 
+  </Route>)
+  );
   return <RouterProvider router={router}/>
 }
 
